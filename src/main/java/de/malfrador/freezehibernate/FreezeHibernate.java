514c695864de11683592dff616ef5a53ce9f127d
Malfrador <malfrador@gmail.com>
2024-06-07 04:06:36 +0200

    Initial commit

---
+++ src/main/java/de/malfrador/freezehibernate/FreezeHibernate.java
@@ -0,0 +1,17 @@
+package de.malfrador.freezehibernate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FreezeHibernate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
