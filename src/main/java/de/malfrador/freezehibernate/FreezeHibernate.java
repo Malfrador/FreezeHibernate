ff817f3a6e8f215a8f3bc357522c17b2132a5813
Malfrador <malfrador@gmail.com>
2024-06-07 04:35:35 +0200

    Use variables instead of statics

--- src/main/java/de/malfrador/freezehibernate/FreezeHibernate.java
+++ src/main/java/de/malfrador/freezehibernate/FreezeHibernate.java
@@ -1,6 +1,8 @@
 package de.malfrador.freezehibernate;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.ServerTickManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,6 +12,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FreezeHibernate extends JavaPlugin implements Listener {
 
+    private final ServerTickManager serverTickManager = Bukkit.getServerTickManager();
+    private final Server server = Bukkit.getServer();
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -17,16 +22,16 @@ public final class FreezeHibernate extends JavaPlugin implements Listener {
 
    @EventHandler
     private void onQuit(PlayerQuitEvent event) {
-        if (Bukkit.getServer().getOnlinePlayers().size() == 1) {
-            Bukkit.getServerTickManager().setFrozen(true);
+        if (server.getOnlinePlayers().size() == 1) {
+            serverTickManager.setFrozen(true);
             getLogger().info("Last player disconnected. Server is now frozen.");
         }
    }
 
    @EventHandler
    private void onJoin(PlayerJoinEvent event){
-         if (Bukkit.getServerTickManager().isFrozen()) {
-              Bukkit.getServerTickManager().setFrozen(false);
+         if (serverTickManager.isFrozen()) {
+              serverTickManager.setFrozen(false);
               getLogger().info("A player joined. Server is now unfrozen.");
          }
    }
@@ -36,8 +41,8 @@ public final class FreezeHibernate extends JavaPlugin implements Listener {
         if (event.getType() == ServerLoadEvent.LoadType.RELOAD) {
             return;
         }
-        if (Bukkit.getServer().getOnlinePlayers().size() == 0) {
-            Bukkit.getServerTickManager().setFrozen(true);
+        if (server.getOnlinePlayers().isEmpty()) {
+            serverTickManager.setFrozen(true);
             getLogger().info("Server is frozen until a player joins.");
         }
     }
